36b0af772f2a74fd3dfc177e60822e7dbc89f028
Arvind Singh Rawat <39412688+ArvindSinghRawat@users.noreply.github.com>
2023-02-25 18:54:47 +0530

    Added python output's

---
+++ docs/schemas/trie_pb2.pyi
@@ -0,0 +1,43 @@
+from google.protobuf.internal import containers as _containers
+from google.protobuf.internal import enum_type_wrapper as _enum_type_wrapper
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+ABBREVIATION: WordType
+ADJECTIVE: WordType
+DESCRIPTOR: _descriptor.FileDescriptor
+NOUN: WordType
+VERB: WordType
+
+class Detail(_message.Message):
+    __slots__ = ["index", "meaning", "type"]
+    INDEX_FIELD_NUMBER: _ClassVar[int]
+    MEANING_FIELD_NUMBER: _ClassVar[int]
+    TYPE_FIELD_NUMBER: _ClassVar[int]
+    index: int
+    meaning: str
+    type: WordType
+    def __init__(self, meaning: _Optional[str] = ..., type: _Optional[_Union[WordType, str]] = ..., index: _Optional[int] = ...) -> None: ...
+
+class Trie(_message.Message):
+    __slots__ = ["children", "detail", "level", "value"]
+    class ChildrenEntry(_message.Message):
+        __slots__ = ["key", "value"]
+        KEY_FIELD_NUMBER: _ClassVar[int]
+        VALUE_FIELD_NUMBER: _ClassVar[int]
+        key: str
+        value: Trie
+        def __init__(self, key: _Optional[str] = ..., value: _Optional[_Union[Trie, _Mapping]] = ...) -> None: ...
+    CHILDREN_FIELD_NUMBER: _ClassVar[int]
+    DETAIL_FIELD_NUMBER: _ClassVar[int]
+    LEVEL_FIELD_NUMBER: _ClassVar[int]
+    VALUE_FIELD_NUMBER: _ClassVar[int]
+    children: _containers.MessageMap[str, Trie]
+    detail: Detail
+    level: int
+    value: str
+    def __init__(self, value: _Optional[str] = ..., detail: _Optional[_Union[Detail, _Mapping]] = ..., children: _Optional[_Mapping[str, Trie]] = ..., level: _Optional[int] = ...) -> None: ...
+
+class WordType(int, metaclass=_enum_type_wrapper.EnumTypeWrapper):
+    __slots__ = []
--- docs/scripts/ts_gen.bat
+++ docs/scripts/ts_gen.bat
@@ -1,1 +1,2 @@
 protoc docs\schemas\trie.proto -I=docs\schemas --ts_out="src/classes/proto/trie" --dependency_out="src/classes/proto/trie/dependency.txt" --enable_codegen_trace=true
+python -m grpc_tools.protoc -I=docs\schemas --python_out="docs/schemas" --pyi_out="docs/schemas" docs/schemas/trie.proto
